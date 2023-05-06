393b03b0c10d5f4b293e54a551fe410893d5a37b
ram <ram@example.com>
2023-05-06 15:19:13 +0000

    amended cur_join file

--- models/curation/cur_join.sql
+++ models/curation/cur_join.sql
@@ -1,5 +1,7 @@
+-- depends_on: {{ ref('stg_table1') }}
+
 {{ config(
-    materialized='incremental',
+    materialized='table',
     partition_by={
       "field": "created_at",
       "data_type": "timestamp",
@@ -8,7 +10,17 @@
     }
 )}}
 
-with monthly as (
+{%- set source_relation = adapter.get_relation(
+      database=source('dbt_test_1', 'raw_table1').database,
+      schema=source('dbt_test_1', 'raw_table1').schema,
+      identifier=source('dbt_test_1', 'raw_table1').name) -%}
+
+{% set table_exists=source_relation is not none %}
+
+{% if table_exists %}
+
+({{ log("Table exists", info=True) }}
+    with monthly as (
     select * from {{ref('stg_table1')}}
    ),
 
@@ -17,14 +29,13 @@ history as (
     where created_at IN (
         select MAX(created_at) 
         from {{ref('cur_table4')}}
-),
-
+)),
 
 final as (
     
     select
-      
         monthly.ID,
+        CURRENT_DATETIME as created_at,
         monthly.Amount,
         monthly.Ind_Latest,
         history.Ind_M1,
@@ -33,6 +44,16 @@ final as (
     from monthly
     join history 
     on monthly.ID = history.ID
-
 )
+
+select * from final)
+{% else %}
+{{ log("Table does not exist", info=True) }}
+with final as (
+select * from {{ref('cur_table4')}}    
+)
+
 select * from final
+{% endif %}
+
+
